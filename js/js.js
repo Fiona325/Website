847b3ef71e160d125c8cf707ce6dc13026f6b15d
chengzhengfeng <631781190@qq.com>
2017-02-21 13:42:40 +0800

    no message

--- js/js.js
+++ js/js.js
@@ -1,21 +1,28 @@
 $(function(){
     $(".index_nav_ul li").hover(function(){
-        $(this).addClass("index_nav_ul_on")
+        $(this).addClass("index_nav_ul_on");
     },function(){
-        $(this).removeClass("index_nav_ul_on")
-    })
+        $(this).removeClass("index_nav_ul_on");
+    });
 
+    $(".case_left_ul1 a").hover(function () {
+        $(this).addClass("case_left_li_on");
+    },function () {
+        $(this).removeClass("case_left_li_on");
+
+    });
+    
     // index_style3
     var obj1=$(".index_style3_l>ul>li");
-    var obj2=$(".index_style3_l_box1")
-    obj2.not(":first").hide()
+    var obj2=$(".index_style3_l_box1");
+    obj2.not(":first").hide();
     for(var i=0;i<obj1.length;i++){
-        a(obj1.eq(i),obj2.eq(i))
+        a(obj1.eq(i),obj2.eq(i));
     }
-})
+});
 var a=function(obj1,obj2){
     obj1.click(function(){
         $(this).addClass("index_style3_l_on").siblings().removeClass("index_style3_l_on");
         obj2.show().siblings().hide();
-    })
-}
+    });
+};
